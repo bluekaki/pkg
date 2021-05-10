09df982d8d2ffe6ed4b3724a4f7bec3aa05a1057
byepichi <83155979+byepichi@users.noreply.github.com>
2021-05-10 16:02:20 +0800

    customize vv error handler

--- vv/builder/gateway/gateway.go
+++ vv/builder/gateway/gateway.go
@@ -4,9 +4,13 @@ import (
 	"bytes"
 	"context"
 	"crypto/rand"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/textproto"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/byepichi/pkg/minami58"
@@ -17,9 +21,11 @@ import (
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/resolver/dns"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/encoding/protojson"
 )
 
@@ -104,11 +110,87 @@ func New(logger *zap.Logger, options ...Option) (*runtime.ServeMux, []grpc.DialO
 		dialTimeout = opt.dialTimeout
 	}
 
+	// copy from runtime.DefaultHTTPErrorHandler
+	errHandler := func(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, r *http.Request, err error) {
+		// return Internal when Marshal failed
+		const fallback = `{"code": 13, "message": "failed to marshal error message"}`
+
+		s := status.Convert(err)
+		pb := s.Proto()
+
+		w.Header().Del("Trailer")
+		w.Header().Del("Transfer-Encoding")
+
+		contentType := marshaler.ContentType(pb)
+		w.Header().Set("Content-Type", contentType)
+
+		buf, merr := marshaler.Marshal(pb)
+		if merr != nil {
+			grpclog.Infof("Failed to marshal error message %q: %v", s, merr)
+			w.WriteHeader(http.StatusInternalServerError)
+			if _, err := io.WriteString(w, fallback); err != nil {
+				grpclog.Infof("Failed to write response: %v", err)
+			}
+			return
+		}
+
+		md, ok := runtime.ServerMetadataFromContext(ctx)
+		if !ok {
+			grpclog.Infof("Failed to extract ServerMetadata from context")
+		}
+
+		for k, vs := range md.HeaderMD {
+			if h, ok := runtime.DefaultHeaderMatcher(k); ok {
+				for _, v := range vs {
+					w.Header().Add(h, v)
+				}
+			}
+		}
+
+		// RFC 7230 https://tools.ietf.org/html/rfc7230#section-4.1.2
+		// Unless the request includes a TE header field indicating "trailers"
+		// is acceptable, as described in Section 4.3, a server SHOULD NOT
+		// generate trailer fields that it believes are necessary for the user
+		// agent to receive.
+		var wantsTrailers bool
+
+		if te := r.Header.Get("TE"); strings.Contains(strings.ToLower(te), "trailers") {
+			wantsTrailers = true
+			for k := range md.TrailerMD {
+				tKey := textproto.CanonicalMIMEHeaderKey(fmt.Sprintf("%s%s", runtime.MetadataTrailerPrefix, k))
+				w.Header().Add("Trailer", tKey)
+			}
+			w.Header().Set("Transfer-Encoding", "chunked")
+		}
+
+		st := runtime.HTTPStatusFromCode(s.Code())
+		if code := strconv.Itoa(int(s.Code())); len(code) == 6 {
+			hst, _ := strconv.Atoi(code[:3])
+			if http.StatusText(hst) != "" {
+				st = hst
+			}
+		}
+
+		w.WriteHeader(st)
+		if _, err := w.Write(buf); err != nil {
+			grpclog.Infof("Failed to write response: %v", err)
+		}
+
+		if wantsTrailers {
+			for k, vs := range md.TrailerMD {
+				tKey := fmt.Sprintf("%s%s", runtime.MetadataTrailerPrefix, k)
+				for _, v := range vs {
+					w.Header().Add(tKey, v)
+				}
+			}
+		}
+	}
+
 	mux := runtime.NewServeMux(
 		runtime.WithIncomingHeaderMatcher(runtime.DefaultHeaderMatcher),
 		runtime.WithOutgoingHeaderMatcher(runtime.DefaultHeaderMatcher),
 		runtime.WithMetadata(annotator),
-		runtime.WithErrorHandler(runtime.DefaultHTTPErrorHandler),
+		runtime.WithErrorHandler(errHandler),
 		runtime.WithStreamErrorHandler(runtime.DefaultStreamErrorHandler),
 		runtime.WithRoutingErrorHandler(runtime.DefaultRoutingErrorHandler),
 		runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.HTTPBodyMarshaler{
