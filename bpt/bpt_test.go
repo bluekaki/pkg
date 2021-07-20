2276e476ced88e5c7bf917eb1423da268c6defeb
byepichi <83155979+byepichi@users.noreply.github.com>
2021-07-20 20:40:16 +0800

    optimize memory allocation in bpt func Add

--- bpt/bpt_test.go
+++ bpt/bpt_test.go
@@ -2,8 +2,10 @@ package bpt
 
 import (
 	"fmt"
+	"math/rand"
 	"strconv"
 	"testing"
+	"time"
 
 	"github.com/bluekaki/pkg/stringutil"
 )
@@ -28,21 +30,39 @@ func (x value) Compare(v Value) stringutil.Diff {
 
 func TestMain(m *testing.M) {
 	SetN(5)
-
 	fmt.Println("_N:", _N, "_Mid:", _Mid, "_T:", _T)
+
+	rand.Seed(time.Now().UnixNano())
+
 	m.Run()
 }
 
 func TestBPT(t *testing.T) {
-	tree := New()
+	if true {
+		tree := New()
 
-	for k := 0; k < 20; k++ {
-		tree.Add(value(k))
+		values := rand.Perm(40)
+		for i := range values[:20] {
+			values[i] = -values[i]
+		}
+
+		for _, v := range values {
+			tree.Add(value(v))
+		}
 		fmt.Println(tree)
 	}
 
-	for k := -20; k <= 0; k++ {
-		tree.Add(value(k))
-		fmt.Println(tree)
+	if false {
+		tree := New()
+
+		for k := 0; k < 20; k++ {
+			tree.Add(value(k))
+			fmt.Println(tree)
+		}
+
+		for k := -20; k <= 0; k++ {
+			tree.Add(value(k))
+			fmt.Println(tree)
+		}
 	}
 }
