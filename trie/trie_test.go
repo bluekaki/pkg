e4345ed60aad7d8b0e1ddc8f52dacaa39230b04f
byepichi <83155979+byepichi@users.noreply.github.com>
2022-02-10 11:36:15 +0800

    define interface for trie

--- trie/trie_test.go
+++ trie/trie_test.go
@@ -5,15 +5,29 @@ import (
 )
 
 func TestTrie(t *testing.T) {
-	trie := new(Trie)
+	trie := New()
 
-	trie.Insert("a")
-	trie.Insert("ab")
-	trie.Insert("abc")
-	trie.Insert("b")
-	trie.Insert("bd")
-	trie.Insert("bde")
-	trie.Insert("bdf")
+	trie.Insert([]string{"a"})
+	t.Log(trie.Capacity(), "\n"+trie.String(EmptyDelimiter))
 
-	t.Log("\n" + trie.String())
+	trie.Insert([]string{"a", "b"})
+	t.Log(trie.Capacity(), "\n"+trie.String(EmptyDelimiter))
+
+	trie.Insert([]string{"a", "b", "c"})
+	t.Log(trie.Capacity(), "\n"+trie.String(EmptyDelimiter))
+
+	trie.Insert([]string{"b"})
+	t.Log(trie.Capacity(), "\n"+trie.String(EmptyDelimiter))
+
+	trie.Insert([]string{"b", "d"})
+	t.Log(trie.Capacity(), "\n"+trie.String(EmptyDelimiter))
+
+	trie.Insert([]string{"b", "d", "e"})
+	t.Log(trie.Capacity(), "\n"+trie.String(EmptyDelimiter))
+
+	trie.Insert([]string{"b", "d", "f"})
+	t.Log(trie.Capacity(), "\n"+trie.String(EmptyDelimiter))
+
+	t.Log(trie.Prompt([]string{"a"}, EmptyDelimiter))
+
 }
