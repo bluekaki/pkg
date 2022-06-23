c74df2516c076c470335be280909a69b04fc2000
byepichi <83155979+byepichi@users.noreply.github.com>
2022-06-23 14:29:17 +0800

    memo the process of reslove prometheus metrics

---
+++ pushgateway/testdata/test.go
@@ -0,0 +1,329 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"mime"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/matttproud/golang_protobuf_extensions/pbutil"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus/push"
+	dto "github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/expfmt"
+	"github.com/prometheus/common/model"
+)
+
+const (
+	namespace = "dummy"
+	subsystem = "xxx"
+)
+
+var KeepaliveCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Namespace: namespace,
+	Subsystem: subsystem,
+	Name:      "keepalive_total",
+}, []string{"topic"})
+
+func init() {
+	prometheus.MustRegister(KeepaliveCounter)
+}
+
+func main() {
+	// TextDecoder()
+
+	// go interceptor()
+
+	// go server()
+	// go pusher()
+
+	for range time.NewTicker(time.Second).C {
+		KeepaliveCounter.WithLabelValues("/abc").Inc()
+	}
+}
+
+func pusher() {
+	pusher := push.New("http://127.0.0.1:8080", "pushgateway").
+		Gatherer(prometheus.DefaultRegisterer.(prometheus.Gatherer)).
+		Grouping("mid", "minami")
+
+	for range time.NewTicker(time.Second * 5).C {
+		if err := pusher.Add(); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func server() {
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Println("-------------------------------------------------------------")
+		fmt.Println(req.Method, req.Host, req.RequestURI)
+
+		for k, v := range req.Header {
+			fmt.Println(k, v)
+		}
+
+		var err error
+		var metricFamilies map[string]*dto.MetricFamily
+		ctMediatype, ctParams, ctErr := mime.ParseMediaType(req.Header.Get("Content-Type"))
+		if ctErr == nil && ctMediatype == "application/vnd.google.protobuf" &&
+			ctParams["encoding"] == "delimited" &&
+			ctParams["proto"] == "io.prometheus.client.MetricFamily" {
+			metricFamilies = map[string]*dto.MetricFamily{}
+			for {
+				mf := &dto.MetricFamily{}
+				if _, err = pbutil.ReadDelimited(req.Body, mf); err != nil {
+					if err == io.EOF {
+						err = nil
+					}
+					break
+				}
+				metricFamilies[mf.GetName()] = mf
+			}
+		}
+
+		if err != nil {
+			panic(err)
+		}
+
+		buf := bytes.NewBuffer(nil)
+		for name, metric := range metricFamilies {
+			fmt.Println("_____________________________")
+			fmt.Println(name, metric)
+
+			expfmt.MetricFamilyToText(buf, metric)
+		}
+
+		fmt.Println(">>>>>>>>>>")
+		fmt.Println(buf.String())
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
+
+func interceptor() {
+	handler := promhttp.Handler()
+
+	http.HandleFunc("/metrics", func(_ http.ResponseWriter, req *http.Request) {
+		fmt.Println("-------------------------------------------------------------")
+		fmt.Println(req.Method, req.Host, req.RequestURI)
+
+		w := NewResponseWriter()
+		handler.ServeHTTP(w, req)
+
+		fmt.Println(w.statusCode, w.headers)
+
+		if len(w.headers["Content-Encoding"]) > 0 && strings.Contains(w.headers["Content-Encoding"][0], "gzip") { // call by browser
+			reader, _ := gzip.NewReader(w.payload)
+
+			buf := make([]byte, 1<<20)
+			l, _ := reader.Read(buf)
+			fmt.Println(string(buf[:l]))
+
+		} else { // call by curl
+			fmt.Println(w.payload.String())
+		}
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
+
+func NewResponseWriter() *responseWriter {
+	return &responseWriter{
+		headers: make(http.Header),
+		payload: bytes.NewBuffer(nil),
+	}
+}
+
+type responseWriter struct {
+	headers    http.Header
+	payload    *bytes.Buffer
+	statusCode int
+}
+
+func (r *responseWriter) Header() http.Header {
+	return r.headers
+}
+
+func (r *responseWriter) Write(p []byte) (int, error) {
+	return r.payload.Write(p)
+}
+
+func (r *responseWriter) WriteHeader(statusCode int) {
+	r.statusCode = statusCode
+}
+
+func TextDecoder() {
+	var (
+		in = `
+# HELP dummy_xxx_keepalive_total 
+# TYPE dummy_xxx_keepalive_total counter
+dummy_xxx_keepalive_total{topic="/abc"} 17
+# HELP go_gc_duration_seconds A summary of the pause duration of garbage collection cycles.
+# TYPE go_gc_duration_seconds summary
+go_gc_duration_seconds{quantile="0"} 0
+go_gc_duration_seconds{quantile="0.25"} 0
+go_gc_duration_seconds{quantile="0.5"} 0
+go_gc_duration_seconds{quantile="0.75"} 0
+go_gc_duration_seconds{quantile="1"} 0
+go_gc_duration_seconds_sum 0
+go_gc_duration_seconds_count 0
+# HELP go_goroutines Number of goroutines that currently exist.
+# TYPE go_goroutines gauge
+go_goroutines 7
+# HELP go_info Information about the Go environment.
+# TYPE go_info gauge
+go_info{version="go1.17.5"} 1
+# HELP go_memstats_alloc_bytes Number of bytes allocated and still in use.
+# TYPE go_memstats_alloc_bytes gauge
+go_memstats_alloc_bytes 1.55576e+06
+# HELP go_memstats_alloc_bytes_total Total number of bytes allocated, even if freed.
+# TYPE go_memstats_alloc_bytes_total counter
+go_memstats_alloc_bytes_total 1.55576e+06
+# HELP go_memstats_buck_hash_sys_bytes Number of bytes used by the profiling bucket hash table.
+# TYPE go_memstats_buck_hash_sys_bytes gauge
+go_memstats_buck_hash_sys_bytes 4251
+# HELP go_memstats_frees_total Total number of frees.
+# TYPE go_memstats_frees_total counter
+go_memstats_frees_total 0
+# HELP go_memstats_gc_sys_bytes Number of bytes used for garbage collection system metadata.
+# TYPE go_memstats_gc_sys_bytes gauge
+go_memstats_gc_sys_bytes 3.51188e+06
+# HELP go_memstats_heap_alloc_bytes Number of heap bytes allocated and still in use.
+# TYPE go_memstats_heap_alloc_bytes gauge
+go_memstats_heap_alloc_bytes 1.55576e+06
+# HELP go_memstats_heap_idle_bytes Number of heap bytes waiting to be used.
+# TYPE go_memstats_heap_idle_bytes gauge
+go_memstats_heap_idle_bytes 2.29376e+06
+# HELP go_memstats_heap_inuse_bytes Number of heap bytes that are in use.
+# TYPE go_memstats_heap_inuse_bytes gauge
+go_memstats_heap_inuse_bytes 1.572864e+06
+# HELP go_memstats_heap_objects Number of allocated objects.
+# TYPE go_memstats_heap_objects gauge
+go_memstats_heap_objects 11032
+# HELP go_memstats_heap_released_bytes Number of heap bytes released to OS.
+# TYPE go_memstats_heap_released_bytes gauge
+go_memstats_heap_released_bytes 2.29376e+06
+# HELP go_memstats_heap_sys_bytes Number of heap bytes obtained from system.
+# TYPE go_memstats_heap_sys_bytes gauge
+go_memstats_heap_sys_bytes 3.866624e+06
+# HELP go_memstats_last_gc_time_seconds Number of seconds since 1970 of last garbage collection.
+# TYPE go_memstats_last_gc_time_seconds gauge
+go_memstats_last_gc_time_seconds 0
+# HELP go_memstats_lookups_total Total number of pointer lookups.
+# TYPE go_memstats_lookups_total counter
+go_memstats_lookups_total 0
+# HELP go_memstats_mallocs_total Total number of mallocs.
+# TYPE go_memstats_mallocs_total counter
+go_memstats_mallocs_total 11032
+# HELP go_memstats_mcache_inuse_bytes Number of bytes in use by mcache structures.
+# TYPE go_memstats_mcache_inuse_bytes gauge
+go_memstats_mcache_inuse_bytes 4800
+# HELP go_memstats_mcache_sys_bytes Number of bytes used for mcache structures obtained from system.
+# TYPE go_memstats_mcache_sys_bytes gauge
+go_memstats_mcache_sys_bytes 16384
+# HELP go_memstats_mspan_inuse_bytes Number of bytes in use by mspan structures.
+# TYPE go_memstats_mspan_inuse_bytes gauge
+go_memstats_mspan_inuse_bytes 28832
+# HELP go_memstats_mspan_sys_bytes Number of bytes used for mspan structures obtained from system.
+# TYPE go_memstats_mspan_sys_bytes gauge
+go_memstats_mspan_sys_bytes 32768
+# HELP go_memstats_next_gc_bytes Number of heap bytes when next garbage collection will take place.
+# TYPE go_memstats_next_gc_bytes gauge
+go_memstats_next_gc_bytes 4.473924e+06
+# HELP go_memstats_other_sys_bytes Number of bytes used for other system allocations.
+# TYPE go_memstats_other_sys_bytes gauge
+go_memstats_other_sys_bytes 714029
+# HELP go_memstats_stack_inuse_bytes Number of bytes in use by the stack allocator.
+# TYPE go_memstats_stack_inuse_bytes gauge
+go_memstats_stack_inuse_bytes 327680
+# HELP go_memstats_stack_sys_bytes Number of bytes obtained from system for stack allocator.
+# TYPE go_memstats_stack_sys_bytes gauge
+go_memstats_stack_sys_bytes 327680
+# HELP go_memstats_sys_bytes Number of bytes obtained from system.
+# TYPE go_memstats_sys_bytes gauge
+go_memstats_sys_bytes 8.473616e+06
+# HELP go_threads Number of OS threads created.
+# TYPE go_threads gauge
+go_threads 7
+# HELP process_cpu_seconds_total Total user and system CPU time spent in seconds.
+# TYPE process_cpu_seconds_total counter
+process_cpu_seconds_total 0
+# HELP process_max_fds Maximum number of open file descriptors.
+# TYPE process_max_fds gauge
+process_max_fds 65535
+# HELP process_open_fds Number of open file descriptors.
+# TYPE process_open_fds gauge
+process_open_fds 9
+# HELP process_resident_memory_bytes Resident memory size in bytes.
+# TYPE process_resident_memory_bytes gauge
+process_resident_memory_bytes 8.445952e+06
+# HELP process_start_time_seconds Start time of the process since unix epoch in seconds.
+# TYPE process_start_time_seconds gauge
+process_start_time_seconds 1.65589390795e+09
+# HELP process_virtual_memory_bytes Virtual memory size in bytes.
+# TYPE process_virtual_memory_bytes gauge
+process_virtual_memory_bytes 1.10712832e+09
+# HELP process_virtual_memory_max_bytes Maximum amount of virtual memory available in bytes.
+# TYPE process_virtual_memory_max_bytes gauge
+process_virtual_memory_max_bytes 1.8446744073709552e+19
+# HELP promhttp_metric_handler_requests_in_flight Current number of scrapes being served.
+# TYPE promhttp_metric_handler_requests_in_flight gauge
+promhttp_metric_handler_requests_in_flight 1
+# HELP promhttp_metric_handler_requests_total Total number of scrapes by HTTP status code.
+# TYPE promhttp_metric_handler_requests_total counter
+promhttp_metric_handler_requests_total{code="200"} 0
+promhttp_metric_handler_requests_total{code="500"} 0
+promhttp_metric_handler_requests_total{code="503"} 0
+`
+		ts = model.Now()
+	)
+
+	dec := &expfmt.SampleDecoder{
+		Dec: expfmt.NewDecoder(strings.NewReader(in), expfmt.FmtProtoText),
+		Opts: &expfmt.DecodeOptions{
+			Timestamp: ts,
+		},
+	}
+
+	for {
+		mf := new(dto.MetricFamily)
+		err := dec.Dec.Decode(mf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("---------------------------")
+		fmt.Println(mf.String())
+	}
+	return
+
+	var all model.Vector
+	for {
+		var smpls model.Vector
+		err := dec.Decode(&smpls)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		all = append(all, smpls...)
+	}
+
+	for i, vec := range all {
+		fmt.Println(i, vec.Metric)
+	}
+}
