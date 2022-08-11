66dae277e2798290cdfb4033d5fa8df86f0ef198
byepichi <83155979+byepichi@users.noreply.github.com>
2022-08-11 17:48:49 +0800

    describe pushgateway metrics stats

---
+++ pushgateway/stats/stats.go
@@ -0,0 +1,101 @@
+package stats
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"sort"
+	"time"
+
+	"github.com/bluekaki/pkg/errors"
+)
+
+type _Label struct {
+	Job          string `json:"job"`
+	Project      string `json:"project"`
+	PodName      string `json:"podname"`
+	JobCreatedAt *_Time `json:"job_created_at"`
+}
+
+func (l *_Label) String() string {
+	return fmt.Sprintf("job: %s, project: %s, podname: %s, job_created_at: %s", l.Job, l.Project, l.PodName, l.JobCreatedAt)
+}
+
+type _Time struct {
+	val time.Time
+}
+
+func (t *_Time) MarshalJSON() ([]byte, error) {
+	return []byte(t.val.Format(time.RFC3339)), nil
+}
+
+func (t *_Time) UnmarshalJSON(raw []byte) error {
+	ts, err := time.Parse(time.RFC3339, string(raw[1:len(raw)-1]))
+	if err != nil {
+		return err
+	}
+
+	t.val = ts
+	return nil
+}
+
+func (t *_Time) String() string {
+	val, _ := t.MarshalJSON()
+	return string(val)
+}
+
+// http://127.0.0.1:9091/api/v1/metrics
+func ParseMetrics(metrics []byte) ([]byte, error) {
+	targets := &struct {
+		Data []struct {
+			Label *_Label `json:"labels"`
+		} `json:"data"`
+	}{}
+	if err := json.Unmarshal(metrics, targets); err != nil {
+		return nil, errors.Wrap(err, "unmarshal json format metrics err")
+	}
+
+	buffer := bytes.NewBuffer(nil)
+	groups := make(map[string][]*_Label)
+
+	for _, target := range targets.Data {
+		if target.Label.Job != "metrics-porter" {
+			buffer.WriteString(target.Label.String())
+			buffer.WriteString("\n")
+			continue
+		}
+
+		groups[target.Label.Project] = append(groups[target.Label.Project], target.Label)
+	}
+
+	if buffer.Len() > 0 {
+		buffer.WriteString("\n")
+		buffer.WriteString("--------------------------------------------------")
+		buffer.WriteString("\n\n")
+	}
+
+	projects := make([]string, 0, len(groups))
+	for project := range groups {
+		projects = append(projects, project)
+	}
+
+	sort.Strings(projects)
+	for _, project := range projects {
+		labels := groups[project]
+		sort.Slice(labels, func(i, j int) bool {
+			return labels[i].JobCreatedAt.val.Before(labels[j].JobCreatedAt.val)
+		})
+
+		for i, labels := range labels {
+			buffer.WriteString(fmt.Sprintf("[%d]  %s", i, labels.String()))
+			buffer.WriteString("\n")
+		}
+		buffer.WriteString(fmt.Sprintf("{%.2f minutes}\n", labels[len(labels)-1].JobCreatedAt.val.Sub(labels[0].JobCreatedAt.val).Minutes()))
+
+		buffer.WriteString("\n")
+		buffer.WriteString("--------------------------------------------------")
+		buffer.WriteString("\n\n")
+	}
+
+	return buffer.Bytes(), nil
+}
---
+++ pushgateway/stats/stats_test.go
@@ -0,0 +1,24 @@
+package stats
+
+import (
+	"testing"
+	"time"
+
+	"github.com/bluekaki/pkg/mm/httpclient"
+)
+
+func TestParseMetrics(t *testing.T) {
+	body, _, _, err := httpclient.Get("http://127.0.0.1:9091/api/v1/metrics", nil,
+		httpclient.WithRetryTimes(0),
+		httpclient.WithTTL(time.Minute))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	info, err := ParseMetrics(body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(string(info))
+}
